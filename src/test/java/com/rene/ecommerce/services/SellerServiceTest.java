18896dd494eb71a307c289da072a8257f4320d7b
kccarlos <kccarlos.an@gmail.com>
2023-04-23 01:54:28 -0400

    Add SellerServiceTest.java Unit mock testing for SellerServiceTest. (NOT FINISHED)

---
+++ src/test/java/com/rene/ecommerce/services/SellerServiceTest.java
@@ -0,0 +1,180 @@
+package com.rene.ecommerce.services;
+
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import com.rene.ecommerce.exceptions.ClientOrSellerHasThisSameEntryException;
+import com.rene.ecommerce.exceptions.DuplicateEntryException;
+import com.rene.ecommerce.repositories.ClientRepository;
+import com.rene.ecommerce.repositories.SellerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.MockitoAnnotations;
+
+import com.rene.ecommerce.domain.users.Seller;
+import com.rene.ecommerce.exceptions.AuthorizationException;
+import com.rene.ecommerce.exceptions.ObjectNotFoundException;
+import com.rene.ecommerce.security.SellerSS;
+
+public class SellerServiceTest {
+
+    @InjectMocks
+    private SellerService sellerService;
+
+    @Mock
+    private SellerRepository sellerRepo;
+
+    @Mock
+    private ClientRepository clientRepo;
+
+    @Mock
+    private UserService userService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testFindById_Success() {
+        // Create a mock seller
+        Seller seller = new Seller();
+        seller.setId(1);
+
+        // Mock SellerSS to simulate authenticated seller
+        SellerSS sellerSS = new SellerSS();
+        sellerSS.setId(1);
+
+        // Define behavior of mocked methods
+        try (MockedStatic<UserService> userService = mockStatic(UserService.class)) {
+            userService.when(UserService::sellerAuthenticated).thenReturn(sellerSS);
+            when(sellerRepo.findById(sellerSS.getId())).thenReturn(Optional.of(seller));
+
+            // Call the method under test
+            Seller foundSeller = sellerService.findById(sellerSS.getId());
+
+            // Assert that the correct seller is returned
+            assertEquals(seller, foundSeller);
+        }
+    }
+
+    @Test
+    public void testFindById_AuthorizationException() {
+        // Mock SellerSS to simulate unauthorized seller
+        SellerSS sellerSS = new SellerSS();
+
+        // Define behavior of mocked methods
+        try (MockedStatic<UserService> userService = mockStatic(UserService.class)) {
+            userService.when(UserService::sellerAuthenticated).thenReturn(null); // should return null?
+            when(sellerRepo.findById(2)).thenReturn(Optional.of(new Seller()));
+
+            // Assert that the AuthorizationException is thrown
+            assertThrows(AuthorizationException.class, () -> {
+                sellerService.findById(2);
+            });
+        }
+    }
+
+    @Test
+    public void testFindById_ObjectNotFoundException() {
+        // Mock SellerSS to simulate authorized seller
+        SellerSS sellerSS = new SellerSS();
+        sellerSS.setId(1);
+
+        // Define behavior of mocked methods
+        try (MockedStatic<UserService> userService = mockStatic(UserService.class)) {
+            userService.when(UserService::sellerAuthenticated).thenReturn(sellerSS);
+            when(sellerRepo.findById(1)).thenReturn(Optional.empty());
+
+            // Assert that the ObjectNotFoundException is thrown
+            assertThrows(ObjectNotFoundException.class, () -> {
+                sellerService.findById(1);
+            });
+        }
+    }
+
+    @Test
+    public void testReturnClientWithoutParsingTheId_Success() {
+        // Create a mock seller
+        Seller seller = new Seller();
+        seller.setId(1);
+
+        // Mock SellerSS to simulate authenticated seller
+        SellerSS sellerSS = new SellerSS();
+        sellerSS.setId(1);
+
+        // Define behavior of mocked methods
+        try (MockedStatic<UserService> userService = mockStatic(UserService.class)) {
+            userService.when(UserService::sellerAuthenticated).thenReturn(sellerSS);
+            when(sellerRepo.findById(sellerSS.getId())).thenReturn(Optional.of(seller));
+
+            // Call the method under test
+            Seller foundSeller = sellerService.returnClientWithoutParsingTheId();
+
+            // Assert that the correct seller is returned
+            assertEquals(seller, foundSeller);
+        }
+    }
+
+    @Test
+    public void testReturnClientWithoutParsingTheId_AuthorizationException() {
+        // Define behavior of mocked methods
+        try (MockedStatic<UserService> userService = mockStatic(UserService.class)) {
+            userService.when(UserService::sellerAuthenticated).thenReturn(null);
+
+            // Assert that the AuthorizationException is thrown
+            assertThrows(AuthorizationException.class, () -> {
+                sellerService.returnClientWithoutParsingTheId();
+            });
+        }
+    }
+
+    @Test
+    public void testReturnClientWithoutParsingTheId_ObjectNotFoundException() {
+        // Mock SellerSS to simulate authorized seller
+        SellerSS sellerSS = new SellerSS();
+        sellerSS.setId(1);
+
+        // Define behavior of mocked methods
+        try (MockedStatic<UserService> userService = mockStatic(UserService.class)) {
+            userService.when(UserService::sellerAuthenticated).thenReturn(sellerSS);
+            when(sellerRepo.findById(sellerSS.getId())).thenReturn(Optional.empty());
+
+            // Assert that the ObjectNotFoundException is thrown
+            assertThrows(ObjectNotFoundException.class, () -> {
+                sellerService.returnClientWithoutParsingTheId();
+            });
+        }
+    }
+
+    @Test
+    public void testFindAll() {
+        // Create mock sellers
+        Seller seller1 = new Seller();
+        seller1.setId(1);
+        Seller seller2 = new Seller();
+        seller2.setId(2);
+
+        // Define a list of sellers to be returned by the repository
+        List<Seller> sellers = Arrays.asList(seller1, seller2);
+
+        // Define behavior of mocked methods
+        when(sellerRepo.findAll()).thenReturn(sellers);
+
+        // Call the method under test
+        List<Seller> foundSellers = sellerService.findAll();
+
+        // Assert that the correct list of sellers is returned
+        assertEquals(sellers, foundSellers);
+    }
+
+}
